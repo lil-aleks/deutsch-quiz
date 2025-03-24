25ad141ed518019edc14ca334aac7566e8c6c87a
lil-aleks <olekiwan4@gmail.com>
2025-03-24 06:38:16 +0100

    all of this was working yesterday

--- middleware.ts
+++ middleware.ts
@@ -6,21 +6,30 @@ import { supabase } from "./lib/supabaseClient";
 export default async function middleware(req: NextRequest) {
   const cookie = await cookies();
   if (
-    req.nextUrl.pathname.startsWith("/admin") &&
-    !req.nextUrl.pathname.endsWith("/login")
+    req.nextUrl.pathname.startsWith("/admin")
   ) {
+    console.log("a");
     const password = cookie.get("password")?.value;
     if (!password || process.env.ADMIN_PASSWORD != password) {
-      return NextResponse.redirect(new URL("/admin/login", req.url));
+      console.log("b");
+      if (!req.nextUrl.pathname.endsWith("/login")) {
+        return NextResponse.redirect(new URL("/admin/login", req.url));
+      } else {
+        return NextResponse.next();
+      }
+    } else {
+      return NextResponse.next();
     }
   }
   
   const username = cookie.get("name")?.value;
   if (!username) {
+    console.log("c");
     if (!req.nextUrl.pathname.endsWith("/")) {
       return NextResponse.redirect(new URL("/", req.url));
     }
   } else {
+    console.log("d");
     if (!req.nextUrl.pathname.startsWith("/admin")) {
       if ((await getUser(username)) !== null) {
         if (!req.nextUrl.pathname.endsWith("/complete")) {
@@ -33,6 +42,7 @@ export default async function middleware(req: NextRequest) {
       }
     }
   }
+  
   
   return NextResponse.next();
 }
