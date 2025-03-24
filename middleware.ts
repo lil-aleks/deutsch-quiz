4c77ea3bd545418ac37c47339f2e3eddf10f16c9
lil aleks <112417784+lil-aleks@users.noreply.github.com>
2025-03-24 07:27:55 +0100

    Update matcher

--- middleware.ts
+++ middleware.ts
@@ -57,5 +57,8 @@ const getUser: (name: string) => Promise<User | null> = async (
 };
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|public/.*\\.(png|svg)$).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image).*)", // Erlaubt alles außer API und Next.js-Assets
+    "!/.*\\.(png|svg|jpg|jpeg|gif|webp|ico|woff|woff2|ttf|eot|otf)$" // Erlaubt statische Medien
+  ],
 };
