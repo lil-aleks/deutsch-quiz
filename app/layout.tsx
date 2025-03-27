47247564e561c65ff044f8c647bda9f481f3a308
lil-aleks <olekiwan4@gmail.com>
2025-03-27 19:21:54 +0100

    copything

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         className="bg-primary w-dvw h-dvh"
       >
         {children}
+        <h1 className="text-gray-400 absolute bottom-1 left-1 text-sm">©Aleksander Iwanowski powered by <a href="https://vercel.com">Vercel</a></h1>
       </body>
     </html>
   );
